4cb0aee32bf3e2a215f6f9b6793fc1fc75a8e838
DimYiannis <johndim99@gmail.com>
2025-07-18 16:41:40 +0300

    c00

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/07 13:45:40 by ydimitra          #+#    #+#             */
+/*   Updated: 2025/06/14 09:10:55 by ydimitra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + '0');
+	}
+}
+
+int	main(void)
+{
+	ft_putnbr(111);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
